2da21d02b704303c7b98d800b9f732333840a6c3
Birute1969 <107874553+Birute1969@users.noreply.github.com>
2022-11-19 22:28:50 +0200

    Home Page button

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Wrapper, Title } from './HomePageStyled';
+import { Wrapper, Title, ButtonDiv } from './HomePageStyled';
+//import { useEffect, useState } from 'react';
 
 const HomePage = () => {
+  
+
+ 
   return (
     <Wrapper>
         <Title>
             Home Page
         </Title>
+        <ButtonDiv type="reset">Get Information</ButtonDiv>
     </Wrapper>
   )
 }
--- src/Pages/HomePage/HomePageStyled.js
+++ src/Pages/HomePage/HomePageStyled.js
@@ -9,3 +9,20 @@ export const Wrapper = styled.div`
     background: rgb(63,94,251);
     background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(19,238,177,0.5494572829131652) 100%);
 `;
+
+export const ButtonDiv = styled.button`
+     width: 300px;
+    height: 36px;
+    color: black;
+    box-sizing: border-box;
+    text-align: center;
+    border-radius: 20px;
+    line-height: 10px;
+    font-size: 18px;
+    font-weight: 600;
+    cursor: pointer;
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(11,221,46,1) 35%, rgba(0,212,255,1) 100%);
+    text-decoration: none;
+    box-shadow: 0 15px 15px rgb(0 42 177 / 12%);
+`;
